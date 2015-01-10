34137b6a4c5ec1ca1995752055967ed2a9cd68a3
Erik Hamrin <eh222ve@student.lnu.se>
2015-01-10 21:24:14 +0100

    Modified Github URL

--- src/model/ProjectRepository.php
+++ src/model/ProjectRepository.php
@@ -20,11 +20,11 @@ class ProjectRepository {
         $php->add(new Project("Gymnastikliga", "http://erikhamrin.se/projects/lnu/1dv404/", "Projekt i kurs Iterativ Mjukvaruutveckling 1DV404 på LNU, grupprojekt där jag ansvarat för backend-utvecklingen"));
 
         $javascript = new ProjectList("Javascript");
-        $javascript->add(new Project("Desktop Application", "http://erikhamrin.se/projects/lnu/1dv403/5-pwd/", "En fönsterhanterare som har ett antal inbyggda applikationer. Detta är slutprojekt i kursen Webteknik I på LNU.", "https://github.com/eh222ve/1dv403-laborationer/tree/master/5-pwd"));
+        $javascript->add(new Project("Desktop Application", "http://erikhamrin.se/projects/lnu/1dv403/5-pwd/", "En fönsterhanterare som har ett antal inbyggda applikationer. Detta är slutprojekt i kursen Webteknik I på LNU.", "https://github.com/ehamrin/1dv403-laborationer/tree/master/5-pwd"));
 
         $csharp = new ProjectList("C#");
-        $csharp->add(new Project("Räkna på löner", "https://github.com/eh222ve/1dv402-eh222ve-1-3-salaries/blob/master/_1DV402.S1.L03C/Program.cs", "En konsolapplikation för uträkning av medianlön, medellön samt spridning."));
-        $csharp->add(new Project("Geometriska former", "https://github.com/eh222ve/1dv402-eh222ve-2-3-shapes/blob/master/_1DV402.S2.L03C/Program.cs", "En konsolapplikation för uträkning mått, volym m.m. på 2D- och 3D-former."));
+        $csharp->add(new Project("Räkna på löner", "https://github.com/ehamrin/1dv402-eh222ve-1-3-salaries/blob/master/_1DV402.S1.L03C/Program.cs", "En konsolapplikation för uträkning av medianlön, medellön samt spridning."));
+        $csharp->add(new Project("Geometriska former", "https://github.com/ehamrin/1dv402-eh222ve-2-3-shapes/blob/master/_1DV402.S2.L03C/Program.cs", "En konsolapplikation för uträkning mått, volym m.m. på 2D- och 3D-former."));
 
         $css = new ProjectList("CSS");
         $css->add(new Project("Bildgalleri", "http://erikhamrin.se/projects/lnu/1ik415/gallery/index.html", "Ett bildgalleri som byggs upp och animeras med CSS"));
